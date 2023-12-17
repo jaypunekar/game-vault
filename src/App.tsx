fbe42e2173d9b5c5115c7bb272fe9f063aac8893
Jay Punekar <jaypunekar01@gmail.com>
2023-12-17 06:50:28 +0530

    build a feature to switch between color mode

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,9 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="blue">aside</GridItem>
+        <GridItem area="aside">aside</GridItem>
       </Show>
-      <GridItem area="main" bg="green">main</GridItem>
+      <GridItem area="main">main</GridItem>
     </Grid>
   )
 }
