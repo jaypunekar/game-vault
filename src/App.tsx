ce0d2dd7d56faf50916e62f87912b2820933b3d5
Jay Punekar <jaypunekar01@gmail.com>
2023-12-20 12:11:32 +0530

    Created a generic Data for fetching hooks i.e. genre and games

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react'
-import { NavBar } from './components/NavBar'
-import GameGrid from './components/GameGrid'
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import { NavBar } from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
   return (
-    <Grid templateAreas={
-      {
+    <Grid
+      templateAreas={{
         base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`
-      }
-    }>
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">aside</GridItem>
+        <GridItem area="aside">
+          <GenreList />
+        </GridItem>
       </Show>
       <GridItem area="main">
         <GameGrid />
       </GridItem>
     </Grid>
-  )
+  );
 }
 
-export default App
+export default App;
