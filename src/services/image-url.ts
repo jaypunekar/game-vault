f25b004f72a7487e9f624b4d0bf6814ed5ffedb7
Jay Punekar <jaypunekar01@gmail.com>
2023-12-21 05:56:57 +0530

    Filter games by genre

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,9 +1,8 @@
-const getCroppedUrl = (url:string) => {
-    const target = 'media/'
-    const index = url.indexOf(target) + target.length;
-    const croppedUrl = url.slice(0, index) + 'crop/600/400/' + url.slice(index);
-    console.log(croppedUrl);
-    return croppedUrl;
-}
+const getCroppedUrl = (url: string) => {
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  const croppedUrl = url.slice(0, index) + "crop/600/400/" + url.slice(index);
+  return croppedUrl;
+};
 
 export default getCroppedUrl;
