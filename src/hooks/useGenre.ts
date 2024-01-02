877ab03453d00fe3f638305cbe71a47d6573f00d
jaypunekar <jay.punekar@icloud.com>
2024-01-02 19:52:33 +0530

    added react query

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,4 +1,7 @@
+import axios from "axios";
 import genres from "../data/genres";
+import { useQuery } from "react-query";
+import useData from "./useData";
 
 export interface Genre {
   id: number;
@@ -7,6 +10,20 @@ export interface Genre {
   slug: string;
 }
 
-const useGenre = () => ({ data: genres, isLoading: false, error: null });
+const useGenre = () => {
+  return useQuery({
+    queryKey: "genres",
+    queryFn: () =>
+      axios
+        .get("https://api.rawg.io/api/", {
+          params: {
+            key: "99e1733bb6bf4a31a8d60fa4d00c7413",
+          },
+        })
+        .then((res) => res.data),
+  });
+};
+
+// const useGenre = () => ({ data: genres, isLoading: false, error: null });
 
 export default useGenre;
